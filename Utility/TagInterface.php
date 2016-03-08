3014a2a35392b431116d2b66772f404f17413b09
Jack Neary <jack.neary@keyretirement.co.uk>
2016-03-08 14:53:54 +0000

    Defined tag interface

---
+++ Utility/TagInterface.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Xeeeveee\Core\Utility;
+
+interface TagInterface {
+
+	public function set_reserved_attributes( array $attributes );
+
+	public function get_reserved_attributes();
+
+	public function set_attributes( array $attributes = [ ] );
+
+	public function add_attributes( array $attributes = [ ], $override = true );
+
+	public function get_attributes();
+
+	public function clear_attributes();
+
+	public function add_class( $class );
+
+	public function add_classes( array $classes = [ ] );
+
+	public function set_class( $class );
+
+	public function set_classes( array $classes = [ ] );
+
+	public function class_exists( $class );
+
+	public function remove_class( $class );
+
+	public function remove_classes( array $classes = [ ] );
+
+	public function clear_classes();
+}

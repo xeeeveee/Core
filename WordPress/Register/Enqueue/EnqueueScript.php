42669d752d09fbbf6f392c3aa2e32859f27dc28b
Jack Neary <xeeeveee@gmail.com>
2016-03-14 20:25:16 +0000

    Added enqueue script

---
+++ WordPress/Register/Enqueue/EnqueueScript.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Xeeeveee\Core\WordPress\Register\Enqueue;
+
+class EnqueueScript extends Enqueue {
+
+	/**
+	 * @var bool
+	 *
+	 * Whether to enqueue the script in the footer
+	 */
+	protected $footer = false;
+
+	/**
+	 * Enqueues the style
+	 */
+	public function enqueue() {
+		if ( ! empty( $this->handle ) && ! empty( $this->source ) ) {
+			wp_enqueue_script( $this->prefix . $this->handle, $this->source, $this->dependencies, $this->version, $this->footer );
+		}
+	}
+}

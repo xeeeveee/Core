be0e7182127ec7eea77d180f5a2b5adcf5bb3206
Jack Neary <xeeeveee@gmail.com>
2016-03-19 22:27:57 +0000

    implemented getters

--- Validation/Validator.php
+++ Validation/Validator.php
@@ -6,14 +6,54 @@ use Xeeeveee\Core\Utility\Singleton;
 
 class Validator extends Singleton implements ValidatorInterface {
 
+	/**
+	 * Holds custom validators
+	 *
+	 * @var array
+	 */
 	protected $validators = [ ];
 
-	protected $error_messages = [
+	/**
+	 * Holds the error templates
+	 *
+	 * @var array
+	 */
+	protected $error_templates = [
 		'string' => ':field must be a string'
 	];
 
+	/**
+	 * Holds the validation errors
+	 *
+	 * @var array
+	 */
+	protected $errors = [ ];
+
+	/**
+	 * Get all the registered validators
+	 *
+	 * @return array
+	 */
 	public function get_validators() {
-		// TODO: Implement get_validators() method.
+		return $this->validators;
+	}
+
+	/**
+	 * Get all errors for the last validator
+	 *
+	 * @return array
+	 */
+	public function get_errors() {
+		return $this->errors;
+	}
+
+	/**
+	 * Get all error templates
+	 *
+	 * @return array
+	 */
+	public function get_error_templates() {
+		return $this->error_templates;
 	}
 
 	public function register_validator( $name, $callback, $error_message, $force = false ) {
@@ -22,12 +62,12 @@ class Validator extends Singleton implements ValidatorInterface {
 			// TODO: Throw exception
 		}
 
-		if ( isset( $this->error_messages[ $name ] )
+		if ( isset( $this->error_templates[ $name ] )
 		     && ( isset( $this->validators[ $name ] ) || method_exists( $this, 'validate_' . $name ) )
 		) {
 			if ( $force ) {
-				$this->validators[ $name ]     = $callback;
-				$this->error_messages[ $name ] = $error_message;
+				$this->validators[ $name ]      = $callback;
+				$this->error_templates[ $name ] = $error_message;
 			} else {
 				// TODO: Throw exception
 			}
@@ -36,10 +76,6 @@ class Validator extends Singleton implements ValidatorInterface {
 
 	public function validate( array $data = [ ], array $rules = [ ] ) {
 		// TODO: Implement validate() method.
-	}
-
-	public function get_errors() {
-		// TODO: Implement get_errors() method.
 	}
 
 	protected function validate_string( $value ) {

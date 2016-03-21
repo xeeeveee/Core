7f11d297e078de88f96a664273f807c85d149e87
Jack Neary <jack.neary@keyretirement.co.uk>
2016-03-21 19:35:11 +0000

    Moved validator to a provider

---
+++ Validation/ValidationProvider.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace Xeeeveee\Core\Validation;
+
+use Xeeeveee\Core\Utility\Singleton;
+
+class ValidationProvider extends Singleton implements ValidationProviderInterface {
+
+	/**
+	 * Holds custom validators
+	 *
+	 * @var array
+	 */
+	protected $validators = [ ];
+
+	/**
+	 * Holds the error templates
+	 *
+	 * @var array
+	 */
+	protected $error_templates = [
+		'string' => ':field must be a string'
+	];
+
+	/**
+	 * Holds the validation errors
+	 *
+	 * @var array
+	 */
+	protected $errors = [ ];
+
+	/**
+	 * Get all the registered validators
+	 *
+	 * @return array
+	 */
+	public function get_validators() {
+		return $this->validators;
+	}
+
+	/**
+	 * Get all errors for the last validator
+	 *
+	 * @return array
+	 */
+	public function get_errors() {
+		return $this->errors;
+	}
+
+	/**
+	 * Get all error templates
+	 *
+	 * @return array
+	 */
+	public function get_error_templates() {
+		return $this->error_templates;
+	}
+
+	public function register_validator( $name, $callback, $error_message, $force = false ) {
+
+		if ( ! is_array( $callback ) && ! is_string( $callback ) ) {
+			// TODO: Throw exception
+		}
+
+		if ( isset( $this->error_templates[ $name ] )
+		     && ( isset( $this->validators[ $name ] ) || method_exists( $this, 'validate_' . $name ) )
+		) {
+			if ( $force ) {
+				$this->validators[ $name ]      = $callback;
+				$this->error_templates[ $name ] = $error_message;
+			} else {
+				// TODO: Throw exception
+			}
+		}
+	}
+
+	public function validate( array $data = [ ], array $rules = [ ] ) {
+		// TODO: Implement validate() method.
+	}
+
+	/**
+	 * Ensure a value exists and is not empty
+	 *
+	 * @param $value
+	 * @param array $parameters
+	 *
+	 * @return bool
+	 */
+	public function validate_required( $value, array $parameters = [ ] ) {
+		return isset( $value ) && ! empty( $value );
+	}
+
+	/**
+	 * Ensure a value exists and is not empty only when other fields are present
+	 *
+	 * @param $value
+	 * @param array $parameters
+	 *
+	 * @return bool
+	 */
+	public function validate_required_with( $value, array $parameters = [ ] ) {
+
+	}
+
+	/**
+	 * Ensure a value exists and is not empty only when other fields are present
+	 *
+	 * @param $value
+	 * @param array $parameters
+	 *
+	 * @return bool
+	 */
+	public function validate_required_without( $value, array $parameters = [ ] ) {
+
+	}
+
+	/**
+	 * Ensure the value is a string
+	 *
+	 * @param $value
+	 * @param array $parameters
+	 *
+	 * @return bool
+	 */
+	protected function validate_string( $value, array $parameters = [ ] ) {
+		return is_string( $value );
+	}
+
+	/**
+	 * Ensure the value is numeric
+	 *
+	 * @param $value
+	 * @param array $parameters
+	 *
+	 * @return bool
+	 */
+	protected function validate_numeric( $value, array $parameters = [ ] ) {
+		return is_numeric( $value );
+	}
+
+	/**
+	 * Ensure the value is an array
+	 *
+	 * @param $value
+	 * @param array $parameters
+	 *
+	 * @return bool
+	 */
+	protected function is_array( $value, array $parameters = [ ] ) {
+		return is_array( $value );
+	}
+
+	/**
+	 * Ensure the value is an object
+	 *
+	 * @param $value
+	 * @param array $parameters
+	 *
+	 * @return bool
+	 */
+	protected function is_object( $value, array $parameters = [ ] ) {
+		return is_object( $value );
+	}
+}
+
---
+++ Validation/ValidationProviderInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Xeeeveee\Core\Validation;
+
+interface ValidationProviderInterface {
+
+	public function get_validators();
+
+	public function get_errors();
+
+	public function get_error_templates();
+
+	public function register_validator( $name, $callback, $error_message );
+
+	public function validate( array $data = [ ], array $rules = [ ] );
+
+
+}

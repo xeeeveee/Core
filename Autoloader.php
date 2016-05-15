986235cf6a63e667cb92cfea91fcec67255a08a0
Jack Neary <xeeeveee@gmail.com>
2016-05-15 23:41:09 +0100

    Added: Support for mu plugins

--- Autoloader.php
+++ Autoloader.php
@@ -11,10 +11,17 @@ spl_autoload_register( function ( $class ) {
 		return;
 	}
 
-	$class       = str_replace( $root . '\\', '', $class );
-	$path        = str_replace( '\\', DIRECTORY_SEPARATOR, $class ) . '.php';
-	$plugin_root = WP_CONTENT_DIR . DIRECTORY_SEPARATOR . 'plugins' . DIRECTORY_SEPARATOR;
-	$theme_root  = WP_CONTENT_DIR . DIRECTORY_SEPARATOR . 'themes' . DIRECTORY_SEPARATOR;
+	$class         = str_replace( $root . '\\', '', $class );
+	$path          = str_replace( '\\', DIRECTORY_SEPARATOR, $class ) . '.php';
+	$plugin_root   = WP_CONTENT_DIR . DIRECTORY_SEPARATOR . 'plugins' . DIRECTORY_SEPARATOR;
+	$muplugin_root = WP_CONTENT_DIR . DIRECTORY_SEPARATOR . 'mu-plugins' . DIRECTORY_SEPARATOR;
+	$theme_root    = WP_CONTENT_DIR . DIRECTORY_SEPARATOR . 'themes' . DIRECTORY_SEPARATOR;
+
+	if ( file_exists( $muplugin_root . $path ) ) {
+		include_once( $muplugin_root . $path );
+
+		return;
+	}
 
 	if ( file_exists( $plugin_root . $path ) ) {
 		include_once( $plugin_root . $path );
